aa6bbc76d004c4871652f55031c434367c8ff3d1
Daniel88V <daniel12velo@gmail.com>
2025-08-11 22:53:03 -0600

    Actividad 15

--- UI-UX.py
+++ UI-UX.py
@@ -16,3 +16,108 @@ def filtrarNom():
             primerLetra = nombre[0]
             if primerLetra in vocales:
                 print(nombre)
+def promedio():
+    Numeros = []
+    print("Ingrese numeros positivos, si desea continuar ingrese un numero negativo")
+    while True:
+        try:
+            Ingreso = input("Ingresa número: ")
+            numero = float(Ingreso)
+        except ValueError:
+            print("Valor incorrecto, intenta de nuevo.")
+            continue
+        if numero > 0:
+            break
+        Numeros.append(numero)
+    if len(Numeros) > 0:
+        suma = sum(Numeros)
+        cantidad = len(Numeros)
+        promedio = suma / cantidad
+        """
+        for num in Numeros:
+            suma += num
+        cantidad = len(Numeros)
+        promedio = suma/cantidad
+        """
+        print(f"La suma de los números positivos es: {suma}")
+        print("La cantidad de numeros es: ", cantidad)
+        print(f"El promedio de los números positivos es: {promedio:.2f}")
+    else:
+        print("No se ingresó ningún número positivo para calcular el promedio.")
+def convertir_temperatura():
+    while True:
+        print("Escoja la conversión que desea efectuar")
+        print("1. Celsius a Fahrenheit")
+        print("2. Fahrenheit a Celsius")
+        print("3. Volver al Menú Principal")
+        grados_opcion = input()
+        match grados_opcion:
+            case "1":
+                try:
+                    print("Ingrese los grados Celsius: ")
+                    celsius = float(input())
+                    fahrenheit = (celsius * 9/5) + 32
+                    print(f"{celsius:.2f}°C a Fahrenheit es: {fahrenheit:.2f}°F")
+                except ValueError:
+                    print("Error, por favor ingrese los grados en números")
+            case "2":
+                while True:
+                    print("Ingrese los grados Fahrenheit: ")
+                    fahrenheit = float(input())
+                    if fahrenheit > 0:
+                        celsius = (fahrenheit - 32) * 5/9
+                        print(f"{fahrenheit:.2f}°F a Celsius es: {celsius:.2f}°C")
+                    elif fahrenheit < 0:
+                        print("Error, por favor ingresar los grados en números")
+                    else:
+                        break
+            case "3":
+                print("Volviendo al menú principal...")
+                break
+            case _:
+                print("Opción no disponible")
+def calcular_area():
+    print(" --- Calcular Área ---")
+    while True:
+        print("Cuadrado - 1")
+        print("Círculo - 2")
+        print("Triángulo - 3")
+        print("Volver al Menú Principal - 4")
+        eleccion = input("Seleccione una opción: ")
+        match eleccion:
+            case "1":
+                while True:
+                    x = float(input("Ingrese el valor de un lado del cuadrado(8, 7.5, 23): "))
+                    if x > 0:
+                        area = x * x
+                        print(f"El área del cuadrado es: {area:.2f}u^2")
+                    elif x < 0:
+                        print("Por favor ingresa un número")
+                    else:
+                        break
+            case "2":
+                while True:
+                    z = float(input("Ingrese el valor del radio: "))
+                    if z > 0:
+                        area = z * z * 3.1416
+                        print(f"El área del círculo es: {area:.2f}u^2")
+                    elif z < 0:
+                        print("Por favor ingresa un número positivo")
+                    else:
+                        break
+            case "3":
+                while True:
+                    y = float(input("Ingrese el valor de la altura: "))
+                    j = float(input("Ingrese el valor de la base: "))
+                    if y > 0 and j > 0:
+                        area = (y * j) / 2
+                        print(f"El área del triángulo es: {area:.2f}u^2")
+                    elif y < 0 or j < 0:
+                        print("Error. Hay un valor que invalida la ecuación, por favor trate de nuevo")
+                    else:
+                        break
+            case "4":
+                print("Volviendo al menú principal...")
+                break
+            case _:
+                print("Opción no disponible")
